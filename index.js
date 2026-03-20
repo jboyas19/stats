1d60ca1542b0425cda0b04267d01d50421e30e69
jboyas19 <jboyas19@yahoo.com>
2026-03-19 19:20:40 -0500

    update stats project

--- index.js
+++ index.js
@@ -1,105 +1,59 @@
-/**
- * @param {number[]} numbers an array of integers
- * @returns {number} the length of the array
- */
 function getLength(numbers) {
-  // TODO
+  return numbers.length;
 }
 
-/**
- * @param {number[]} numbers an array of integers
- * @returns {number} the sum of the numbers
- */
 function getSum(numbers) {
-  // TODO
-}
-
-/**
- * @param {number[]} numbers an array of integers
- * @returns {number} the mean of the numbers
- */
-function getMean(numbers) {
-  // TODO
-}
-
-/**
- * @param {number[]} numbers an array of integers
- * @returns {number} the smallest of the numbers
- */
-function getMin(numbers) {
-  // TODO
-}
-
-/**
- * @param {number[]} numbers an array of integers
- * @returns {number} the largest of the numbers
- */
-function getMax(numbers) {
-  // TODO
-}
-
-/**
- * @param {number[]} numbers an array of integers
- * @returns {number} the range of the numbers (max - min)
- */
-function getRange(numbers) {
-  // TODO
-}
-
-/**
- * @param {number[]} numbers an array of integers
- * @returns {number[]} the even numbers in the array
- */
-function getEvens(numbers) {
-  // TODO
-}
-
-/**
- * @param {number[]} numbers an array of integers
- * @returns {number[]} the odd numbers in the array
- */
-function getOdds(numbers) {
-  // TODO
-}
-
-// === READ BUT DO NOT EDIT THE CODE BELOW ===
-
-/**
- * @param {string} commaSeparatedNumbers
- * @returns {number[]} the string converted into an array of numbers
- */
-function convertStringToNumbers(commaSeparatedNumbers) {
-  // Split the string of numbers into an array of strings.
-  const strings = commaSeparatedNumbers.split(",");
-
-  // Convert the array of strings into an array of numbers
-  const numbers = [];
-  for (const s of strings) {
-    const number = parseInt(s);
-    numbers.push(number);
+  let sum = 0;
+  for (let num of numbers) {
+    sum += num;
   }
-  return numbers;
+  return sum;
 }
 
-/**
- * Prints some descriptive statistics about the provided numbers.
- * @param {number[]} numbers
- */
-function describeNumbers(numbers) {
-  console.log(numbers);
-  console.log(`You have given ${getLength(numbers)} numbers.`);
-  console.log(`The sum of your numbers is ${getSum(numbers)}.`);
-  console.log(`The mean of your numbers is ${getMean(numbers)}.`);
-  console.log(`The smallest of your numbers is ${getMin(numbers)}.`);
-  console.log(`The largest of your numbers is ${getMax(numbers)}.`);
-  console.log(`The range of your numbers is ${getRange(numbers)}.`);
-  console.log(`The even numbers you gave are ${getEvens(numbers)}.`);
-  console.log(`The odd numbers you gave are ${getOdds(numbers)}.`);
+function getMean(numbers) {
+  return getSum(numbers) / getLength(numbers);
 }
 
-const userInputString = prompt(
-  "Please enter some integers separated by commas.",
-  "28,-15,30,975,400"
-);
-const numbers = convertStringToNumbers(userInputString);
-describeNumbers(numbers);
+function getMin(numbers) {
+  let min = numbers[0];
+  for (let num of numbers) {
+    if (num < min) {
+      min = num;
+    }
+  }
+  return min;
+}
+
+function getMax(numbers) {
+  let max = numbers[0];
+  for (let num of numbers) {
+    if (num > max) {
+      max = num;
+    }
+  }
+  return max;
+}
+
+function getRange(numbers) {
+  return getMax(numbers) - getMin(numbers);
+}
+
+function getEvens(numbers) {
+  let evens = [];
+  for (let num of numbers) {
+    if (num % 2 === 0) {
+      evens.push(num);
+    }
+  }
+  return evens;
+}
+
+function getOdds(numbers) {
+  let odds = [];
+  for (let num of numbers) {
+    if (num % 2 !== 0) {
+      odds.push(num);
+    }
+  }
+  return odds;
+}
